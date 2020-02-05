bcf163ead5d27afbcc0041218108c3e921b04abb
Metje Ruberg <metje.ruberg@telekom.de>
2020-02-05 16:15:40 +0100

    Passwort aus DB

--- src/main/java/com/wcs/serialseries/config/WebSecurityConfig.java
+++ src/main/java/com/wcs/serialseries/config/WebSecurityConfig.java
@@ -1,47 +1,31 @@
 package com.wcs.serialseries.config;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.wcs.serialseries.service.UserDetailsServiceImpl;
 
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private final UserDetailsServiceImpl userDetailsService;
+
+	private final PasswordEncoder passwordEncoder;
+	
+	public WebSecurityConfig (UserDetailsServiceImpl userDetailsService, PasswordEncoder passwordEncoder) {
+		this.userDetailsService = userDetailsService;
+		this.passwordEncoder = passwordEncoder;
+	}
 
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-	    http
-	.authorizeRequests()
-		.antMatchers("/listSeries/*", "/listSerieSeasonsEpisodes/**").hasAnyRole("ADMIN","JUNKY")
-		.antMatchers("/", "/startSerialSeries", 
-    		"/listSeries", "/FAQ", "/pictures/*", "/media/*", 
-    		"/FAQ.css", "/global.css", "/header.css", "/listSeries.css", "/header.css", "/listSerieWithSeasonsAndEpisodes.css", "/start.css" ,  
-    		"/webjars/**").permitAll()
-		.anyRequest().authenticated()
-        .and()
-    .formLogin()
-        .and()
-    .httpBasic();
-		
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
 	}
 	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		
-		auth.inMemoryAuthentication()
-	    .withUser("Metje")
-	        .password(encoder.encode("1234"))
-	        .roles("ADMIN")
-	        .and()
-		 .withUser("Ralf")
-	        .password(encoder.encode("1234"))
-	        .roles("JUNKY");
-			
-	}
+	
 	
 }
 
--- src/main/java/com/wcs/serialseries/model/User.java
+++ src/main/java/com/wcs/serialseries/model/User.java
@@ -1,7 +1,9 @@
 package com.wcs.serialseries.model;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import static java.util.Collections.singletonList;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,17 +11,27 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 
 
 @Entity
-public class User {
+public class User implements UserDetails {
+	
+
+	private static final long serialVersionUID = -1987953842734862009L;
+	
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-	Long id;
-	String name;
+	private Long id;
+	private String username;
+	private String password;
 	
-	 @OneToMany(mappedBy = "user")
-	 private List<SerieUser> serieUsers = new ArrayList<>();
+	
+	@OneToMany(mappedBy = "user")
+	private List<SerieUser> serieUsers = new ArrayList<>();
 	 
 	 
 	
@@ -36,13 +48,18 @@ public class User {
 	}
 
 
-	public String getName() {
-		return name;
+	public void setUsername(String name) {
+		this.username = name;
 	}
 
 
-	public void setName(String name) {
-		this.name = name;
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 
@@ -53,6 +70,37 @@ public class User {
 
 	public void setSerieUsers(List<SerieUser> serieUsers) {
 		this.serieUsers = serieUsers;
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_JUNKY");
+		return singletonList(authority);
+	}
+
+	@Override
+	public String getUsername() {
+		return username;
+	}
+		
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}	
+	
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 	
 }
