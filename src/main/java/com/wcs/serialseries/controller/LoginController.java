966852a1ad54528f0b6ae07dca5c25d7aa92571e
Metje Ruberg <metje.ruberg@telekom.de>
2020-02-08 16:04:22 +0100

    Header und Footer zur Loginseite hinzugefügt

--- src/main/java/com/wcs/serialseries/controller/LoginController.java
+++ src/main/java/com/wcs/serialseries/controller/LoginController.java
@@ -1,16 +1,28 @@
 package com.wcs.serialseries.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.wcs.serialseries.repository.SerieRepository;
+import com.wcs.serialseries.service.UserService;
+
 @Controller
 public class LoginController {
+	
+	private final UserService service;
+
+	@Autowired
+	public LoginController(UserService service) {
+		this.service = service;
+	}
 	
 	@GetMapping("/login")
 	public String loginPage(Model model) {
 
-		
+		model.addAttribute("Title", service.getEmptyTitle());
+		model.addAttribute("Type", "Login");
 		return "/login";
 	}
 	
